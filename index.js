7ee3e6e339f2475971a6897c934e4c8d771784f2
fabilqis <fadillah.bilqis@gmail.com>
2018-07-18 16:53:36 +0700

    add delete button

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
-
-$(document).ready(function () {
-  $('input:radio').change(function () {
-  });
-  $("button").click(function () {
-      let text = $("input[type=text]").val();
+$(document).ready(function() {
+  $('input:radio').change(function() {});
+  $("button").click(function() {
+    if ($("input[type=text").val().length === 0){
+      alert("Fill the blank");
+    }
+      var text = $("input[type=text]").val();
       var value = $("input[type='radio']:checked").val();
-      $("ol").append(value+"<p>"+text+"</p>");
+      $("ol").append(value + "<p>" + text + "</p>" + "<button id='deleter' class='btn btn-danger md-1'>X</button>"+ "<hr>" );
     });
   });
 
 
-$(document).on("dblclick", "li", function () {
-  $(this).hide()
+
+$(document).on("click", "#deleter", function(){
+  $("ol").remove();
 })
