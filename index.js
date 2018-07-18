91a870c15044be067a359d06d20a2897c04d8909
fabilqis <fadillah.bilqis@gmail.com>
2018-07-18 17:18:14 +0700

    make new alignment

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 $(document).ready(function() {
   $('input:radio').change(function() {});
   $("button").click(function() {
+    var text = $("input[type=text]").val();
+    var value = $("input[type='radio']:checked").val();
     if ($("input[type=text").val().length === 0){
       alert("Fill the blank");
     }
-      var text = $("input[type=text]").val();
-      var value = $("input[type='radio']:checked").val();
-      $("ol").append(value + "<p>" + text + "</p>" + "<button id='deleter' class='btn btn-danger md-1'>X</button>"+ "<hr>" );
+      $("#add").append("<li>"+ value + "<p>" + text  + "</p>" + "<button id='deleter' class='btn btn-danger md-1'>X</button><hr></li>" );
+      $("input[type=text").val("")
     });
   });
 
 
 
-$(document).on("click", "#deleter", function(){
-  $("ol").remove();
-})
+$(document).on("click", "li", function(){
+  $(this).remove()
+});
