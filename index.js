b91fd27697eee8fb3cb1af4763cb263419429742
fabilqis <fadillah.bilqis@gmail.com>
2018-07-18 10:01:28 +0700

    Make various jQuery program

---
+++ index.js
@@ -0,0 +1,14 @@
+//$(document).ready(function(){
+  //  $("#heading").css("background", "green");
+//});
+
+// $(document).ready(function(){
+//     $(".h1").css("background","green");
+// });
+
+
+$(document).ready(function(){
+    $("a").click(function(event){
+        alert("Thanks for visiting!");
+    });
+});
