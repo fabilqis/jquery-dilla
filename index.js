fe170da20dacfc6db9ee544e22b7415d05d8858d
fabilqis <fadillah.bilqis@gmail.com>
2018-07-18 15:42:54 +0700

    Edit radio button and connecting radio button with todolist.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-//$(document).ready(function(){
-  //  $("#heading").css("background", "green");
-//});
-
-// $(document).ready(function(){
-//     $(".h1").css("background","green");
-// });
 
 
-// $(document).ready(function(){
-//     $("a").click(function(event){
-//         alert("Thanks for visiting!");
-//     });
-// });
+$(document).ready(function () {
+  $('input:radio').change(function () {
+  });
+  $("button").click(function () {
+      let text = $("input[type=text]").val();
+      var value = $("form input[type='radio']:checked").val();
+      $("ol").append(value+"<p>"+text+"</p>");
+    });
+  });
+
+
+$(document).on("dblclick", "li", function () {
+  $(this).hide()
+})
